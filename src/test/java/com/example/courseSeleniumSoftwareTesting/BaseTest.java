dc9b0224abe7f17e875fd62a73e5b8316e42e732
pavel <natitshow@gmail.com>
2021-05-23 15:52:31 +0300

    added hw 3.6

--- src/test/java/com/example/courseSeleniumSoftwareTesting/BaseTest.java
+++ src/test/java/com/example/courseSeleniumSoftwareTesting/BaseTest.java
@@ -1,16 +1,21 @@
 package com.example.courseSeleniumSoftwareTesting;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Cookie;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -22,16 +27,20 @@ public class BaseTest {
     private String browser;
 
     @BeforeMethod
-    public void setUp(@Optional("edge") String browser) {
+    public void setUp(@Optional("chrome") String browser) {
+       DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability("unexpectedAlertBehaviour", "dismiss");
         switch (browser) {
             case "chrome":
                 System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-                driver = new ChromeDriver();
+                ChromeOptions optionsChrome = new ChromeOptions().merge(cap);
+                driver = new ChromeDriver(optionsChrome);
                 break;
             // driver.set(new ChromeDriver());
             case "firefox":
                 System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
-                driver = new FirefoxDriver();
+                FirefoxOptions optionsFire = new FirefoxOptions().setBinary("C:/Program Files/Firefox Nightly/firefox.exe");
+                driver = new FirefoxDriver(optionsFire);
                 break;
             //  driver.set(new FirefoxDriver());
             case "edge":
@@ -39,10 +48,7 @@ public class BaseTest {
                 driver = new EdgeDriver();
                 break;
         }
-//        DesiredCapabilities cap = new DesiredCapabilities();
-//        cap.setCapability("unexpectedAlertBehaviour", "dismiss");
-//        driver = new ChromeDriver(cap);
-//        System.out.println(((HasCapabilities) driver).getCapabilities());
+        System.out.println(((HasCapabilities) driver).getCapabilities());
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://localhost/litecart/litecart-1.3.7/public_html/admin/login.php");
