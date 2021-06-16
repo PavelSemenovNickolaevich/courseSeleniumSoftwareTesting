09659e8c049331d8c365e1517e507b31b3117092
pavel <natitshow@gmail.com>
2021-06-16 22:58:40 +0300

    corrected 9HW

--- src/test/java/com/example/courseSeleniumSoftwareTesting/admin/TestAdmin2.java
+++ src/test/java/com/example/courseSeleniumSoftwareTesting/admin/TestAdmin2.java
@@ -27,55 +27,49 @@ public class TestAdmin2 extends BaseTestAdmin {
     private static final By COUNTRIES_OF_LIST = By.xpath("//form[@name='countries_form']//td[5]//a");
     private static final By ZONES_OF_LIST = By.xpath("//form[@name='countries_form']//td[6]");
     private static final By NAMES_ZONE_OF_LIST = By.xpath("//table[@class='dataTable']//td[3]//input[@type='hidden']");
-    private static final By GEOZONES_LOCATOR=  By.xpath("//span[contains(text(),'Geo Zones')]");
-    private static final By GEOZONES_FROM_GEOZONES_MENU=  By.xpath("//table[@class='dataTable']//td[3]//a");
+    private static final By GEOZONES_LOCATOR = By.xpath("//span[contains(text(),'Geo Zones')]");
+    private static final By GEOZONES_FROM_GEOZONES_MENU = By.xpath("//table[@class='dataTable']//td[3]//a");
 
 
-    @Test (testName = "First test with subtask A and B")
+    @Test(testName = "First test with subtask A and B")
     public void shouldSortCountriesAndGeozons() throws InterruptedException {
         Thread.sleep(1000);
         driver.findElement(COUNTRIES_LOCATOR).click();
         List<WebElement> listCountries = driver.findElements(COUNTRIES_OF_LIST);
         compareAlphabetic(listCountries);
-//        for (int i = 0; i < listCountries.size() - 1; i++) {
-//            if (listCountries.get(i + 1).getText().compareTo(listCountries.get(i).getText()) < 0) {
-//                Assert.fail("Countries list in non-alphabetic order!");
-//            }
-//        }
-
         List<WebElement> listZones = driver.findElements(ZONES_OF_LIST);
-        for (int i = 0; i < listZones.size(); i++) {
-            if(!listZones.get(i).getText().equals("0")) {
+        int size = listZones.size();
+        for (int i = 0; i < size; i++) {
+            if (!listZones.get(i).getText().equals("0")) {
                 listCountries.get(i).click();
-                Thread.sleep(1000);
                 List<WebElement> nameZones = driver.findElements(NAMES_ZONE_OF_LIST);
                 compareAlphabetic(nameZones);
+                Thread.sleep(1000);
                 driver.findElement(By.xpath("//button[@name='cancel']")).click();
+                i++;
             }
+            listCountries = driver.findElements(COUNTRIES_OF_LIST);
+            listZones = driver.findElements(ZONES_OF_LIST);
         }
-     }
+    }
 
     @Test(testName = "Second test")
     public void shouldAlphabeticGeoZones() throws InterruptedException {
         Thread.sleep(1000);
-         driver.findElement(GEOZONES_LOCATOR).click();
-         List<WebElement> elements = driver.findElements(GEOZONES_FROM_GEOZONES_MENU);
-         for (int i = 0; i < elements.size(); i++) {
-             Thread.sleep(2000);
-             elements.get(i).click();
-             List<WebElement> el = driver.findElements(By.xpath("//table[@class='dataTable']//td[3]"));
-             compareAlphabetic(el);
-//             for(int j = 0;  j < el.size() - 1; j++) {
-//                 if (el.get(j + 1).getText().compareTo(el.get(j).getText()) < 0) {
-//                     Assert.fail("Zones list in non-alphabetic order!");
-//                 }
-//             }
-             driver.findElement(By.xpath("//button[@name='cancel']")).click();
-         }
-     }
+        driver.findElement(GEOZONES_LOCATOR).click();
+        List<WebElement> elements = driver.findElements(GEOZONES_FROM_GEOZONES_MENU);
+        for (int i = 0; i < elements.size(); i++) {
+            Thread.sleep(2000);
+            elements.get(i).click();
+            List<WebElement> el = driver.findElements(By.xpath("//table[@class='dataTable']//td[3]//option[@selected='selected']"));
+            compareAlphabetic(el);
+            driver.findElement(By.xpath("//button[@name='cancel']")).click();
+            elements = driver.findElements(GEOZONES_FROM_GEOZONES_MENU);
+        }
+    }
 
     private void compareAlphabetic(List<WebElement> list) {
-        for(int j = 0;  j < list.size() - 1; j++) {
+        for (int j = 0; j < list.size() - 1; j++) {
             if (list.get(j + 1).getText().compareTo(list.get(j).getText()) < 0) {
                 Assert.fail("Zones/Countries list in non-alphabetic order!");
             }
